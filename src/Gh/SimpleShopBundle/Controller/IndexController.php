7d156f69df367447a8ede371eaeb42c9ea7987b0
SPolischook <spolischook@gmail.com>
2013-04-17 11:15:36 +0300

    Create simple index action

---
+++ src/Gh/SimpleShopBundle/Controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Gh\SimpleShopBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class IndexController extends Controller
+{
+    /**
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return new Response('hello world!');
+    }
+}
