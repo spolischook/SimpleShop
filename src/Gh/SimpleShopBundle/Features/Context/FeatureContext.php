fd7899ab594d053e70b7f65f9a4654d3a7902138
SPolischook <spolischook@gmail.com>
2013-04-23 17:09:32 +0300

    Add Behat tests

---
+++ src/Gh/SimpleShopBundle/Features/Context/FeatureContext.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Gh\SimpleShopBundle\Features\Context;
+
+use Symfony\Component\HttpKernel\KernelInterface;
+use Behat\Symfony2Extension\Context\KernelAwareInterface;
+use Behat\MinkExtension\Context\MinkContext;
+use Behat\CommonContexts\WebApiContext;
+
+use Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+
+//
+// Require 3rd-party libraries here:
+//
+//   require_once 'PHPUnit/Autoload.php';
+//   require_once 'PHPUnit/Framework/Assert/Functions.php';
+//
+
+/**
+ * Feature context.
+ */
+class FeatureContext extends BehatContext
+{
+    private $kernel;
+    private $parameters;
+
+    /**
+     * Initializes context with parameters from behat.yml.
+     *
+     * @param array $parameters
+     */
+    public function __construct(array $parameters)
+    {
+        $this->parameters = $parameters;
+        $this->useContext('api',
+            new WebApiContext($parameters['base_url'])
+        );
+    }
+
+    /**
+     * Sets HttpKernel instance.
+     * This method will be automatically called by Symfony2Extension ContextInitializer.
+     *
+     * @param KernelInterface $kernel
+     */
+    public function setKernel(KernelInterface $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+//
+// Place your definition and hook methods here:
+//
+//    /**
+//     * @Given /^I have done something with "([^"]*)"$/
+//     */
+//    public function iHaveDoneSomethingWith($argument)
+//    {
+//        $container = $this->kernel->getContainer();
+//        $container->get('some_service')->doSomethingWith($argument);
+//    }
+//
+}
